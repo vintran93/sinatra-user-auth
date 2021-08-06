0dc998c46f6140ccf758537d20752ea0044cfb69
ihollander <ianholla@gmail.com>
2021-08-06 07:42:36 -0400

    Turn of logging in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,8 @@ if ActiveRecord::Base.connection.migration_context.needs_migration?
   raise 'Migrations are pending run `rake db:migrate SINATRA_ENV=test` to resolve the issue.'
 end
 
+ActiveRecord::Base.logger = nil
+
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
